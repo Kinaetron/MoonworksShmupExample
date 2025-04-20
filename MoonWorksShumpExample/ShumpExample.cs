c3936f9125a613c2477d951f86918692b4c2a478
Darren <darren.daley@hotmail.co.uk>
2025-04-20 13:30:06 +0100

    Got setup done
    The Cornflower blue blooms !

---
+++ MoonWorksShumpExample/ShumpExample.cs
@@ -0,0 +1,35 @@
+using MoonWorks;
+using MoonWorks.Graphics;
+
+namespace MoonWorksShumpExample;
+
+public class ShumpExample : Game
+{
+    public ShumpExample(
+        AppInfo appInfo,
+        WindowCreateInfo windowCreateInfo,
+        FramePacingSettings framePacingSettings
+        ) : base(
+            appInfo,
+            windowCreateInfo,
+            framePacingSettings,
+            ShaderFormat.SPIRV | ShaderFormat.DXIL | ShaderFormat.MSL | ShaderFormat.DXBC)
+    {
+    }
+
+    protected override void Update(TimeSpan delta) { }
+
+    protected override void Draw(double alpha)
+    {
+        CommandBuffer cmdbuf = GraphicsDevice.AcquireCommandBuffer();
+        Texture swapchainTexture = cmdbuf.AcquireSwapchainTexture(MainWindow);
+        if (swapchainTexture != null)
+        {
+            var renderPass = cmdbuf.BeginRenderPass(
+                new ColorTargetInfo(swapchainTexture, Color.CornflowerBlue)
+            );
+            cmdbuf.EndRenderPass(renderPass);
+        }
+        GraphicsDevice.Submit(cmdbuf);
+    }
+}
